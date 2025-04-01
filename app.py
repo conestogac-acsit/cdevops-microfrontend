50f6f9350549690938e3d5e5960d01c2dd4d863a
Rich Hildred <rhildred@gmail.com>
2025-04-01 16:10:14 +0000

    add cors headers

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from random import randrange
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/rolldice")
 async def rolldice():
